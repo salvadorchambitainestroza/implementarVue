5fa5326b385300946807c7dfe998f559d3903dc4
salvadorchambitainestroza <110933237+salvadorchambitainestroza@users.noreply.github.com>
2023-08-27 14:02:50 -0600

    Update principal.js
    agrege codigo de la app

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        // titulo : 'hola mundo'
+        nombre: '',
+        promedio: '',
+        lista:[],
+    },
+    methods:{
+        agregarNota: function () {
+            if (this.name!="" && this.promedio!="") {
+                this.lista.push({nombre: this.nombre, promedio: this.promedio, estado: this.estado()})
+                this.nombre = '';
+                this.promedio = '';
+            } else {
+                alert('Nombre y promedio son obligatorios');
+            }
+        },
+        estado: function() {
+            if (this.promedio >= 7) {
+                return 'Aprobado';
+            } else {
+                return 'Reprobado';
+            }
+        },
+    }
+})
 
